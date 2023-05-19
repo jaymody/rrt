614d1c6e07f317475544f092a0f7e78573767e2a
jaymody <jaykmody@gmail.com>
2023-05-19 18:30:12 -0400

    Use inline flags for wasm-pack build instead of via .cargo/config.toml.

--- .proxyrc.js
+++ .proxyrc.js
@@ -1,3 +1,6 @@
+// wasm-bindgen-rayon (which let's our wasm code us multiple threads) requires
+// the use of SharedArrayBuffer, which requires cross origin isolation policies,
+// see: https://github.com/GoogleChromeLabs/wasm-bindgen-rayon#setting-up
 module.exports = function (app) {
   app.use((req, res, next) => {
     res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
