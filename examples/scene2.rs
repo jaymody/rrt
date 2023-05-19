4456c5a902f2682736308d60a623e50f68bb4ca7
jaymody <jaykmody@gmail.com>
2023-05-18 22:25:43 -0400

    Update examples.

---
+++ examples/scene2.rs
@@ -0,0 +1,22 @@
+use rrt::{
+    color::Color, engine::Engine, material::Lambertian, object::Object, scene::Scene,
+    shape::Sphere, vec3::Vec3,
+};
+
+fn main() {
+    let mut scene = Scene::new();
+    let sphere = Object::new(
+        Box::new(Sphere::new(0.5, Vec3::new(0., 0., -1.))),
+        Box::new(Lambertian::new(Color::RED)),
+    );
+    let ground = Object::new(
+        Box::new(Sphere::new(100.0, Vec3::new(0.0, -100.5, -1.0))),
+        Box::new(Lambertian::new(Color::GREEN)),
+    );
+    scene.add_object(sphere);
+    scene.add_object(ground);
+
+    let engine = Engine::new().scene(scene);
+
+    std::fs::write("./output.ppm", engine.render().to_ppm()).unwrap();
+}
