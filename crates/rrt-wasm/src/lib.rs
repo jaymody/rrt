577ae02193139a39e3a2371bb227a3ea735c13a0
jaymody <jaykmody@gmail.com>
2023-05-19 01:04:04 -0400

    Move dummy image logic to rust wasm crate.

--- crates/rrt-wasm/src/lib.rs
+++ crates/rrt-wasm/src/lib.rs
@@ -1,6 +1,27 @@
+use js_sys::Uint8ClampedArray;
 use wasm_bindgen::prelude::*;
 
+extern crate console_error_panic_hook;
+
 #[wasm_bindgen]
-pub fn greet(name: &str) -> String {
-    format!("Hello {}!", name)
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn render(width: u32, height: u32) -> Uint8ClampedArray {
+    console_error_panic_hook::set_once();
+
+    let arr = Uint8ClampedArray::new_with_length(width * height * 4);
+    for i in 0..height {
+        for j in 0..width {
+            let idx = (i * width + j) * 4;
+            arr.set_index(idx + 0, ((i as f64 / height as f64) * 255.99) as u8);
+            arr.set_index(idx + 1, ((j as f64 / width as f64) * 255.99) as u8);
+            arr.set_index(idx + 2, 0);
+            arr.set_index(idx + 3, 255);
+        }
+    }
+    arr
 }
--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-import init, { greet } from "./crates/rrt-wasm/pkg/rrt_wasm.js";
+import init, { render } from "./crates/rrt-wasm/pkg/rrt_wasm.js";
 
 const main = async () => {
   const { memory } = await init("./crates/rrt-wasm/pkg/rrt_wasm_bg.wasm");
 
-  const height = 400;
-  const width = 300;
+  const width = 400;
+  const height = 225;
 
   const canvas = document.getElementById("canvas");
   canvas.width = width;
   canvas.height = height;
-
   const ctx = canvas.getContext('2d');
-  const imageData = ctx.createImageData(width, height);
 
-  for (let i = 0; i < height; i += 1) {
-    for (let j = 0; j < width; j += 1) {
-      let idx = (i * width + j) * 4;
-      imageData.data[idx + 0] = 255.99 * i / height;
-      imageData.data[idx + 1] = 255.99 * j / width;
-      imageData.data[idx + 2] = 0;
-      imageData.data[idx + 3] = 255;
-    }
-  }
-
+  const dataArray = render(width, height);
+  const imageData = new ImageData(dataArray, width);
   ctx.putImageData(imageData, 0, 0);
 };
 
